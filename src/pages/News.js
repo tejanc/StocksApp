bb13415cef0d31d83bb7a3353c14d4ea30f363b6
Tejan C <tejan_rs@hotmail.com>
2020-09-26 00:58:53 -0400

    Added div for responsive news blog style cards

--- src/pages/News.js
+++ src/pages/News.js
@@ -1,22 +1,113 @@
 import React from 'react';
 
+var article = {
+    author: "",
+    title: "",
+    description: "",
+    url: "",
+    urlToImage: "",
+    publishedAt: "",
+    content: "",
+}
+
 export default class News extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            api: '',
+            query: '',
+            arr: []
+        }
+    }
+
+    componentDidMount() {
+        var rand = Math.random() * Math.pow(10, 17);
+        this.setState({
+            query: rand
+        }, () => this.fetchNews('Amazon'));
+        this.render();
+    }
+
+    fetchNews = async (query) => {
+
+        const pointerToThis = this;
+
+        let proxy = 'https://cors-anywhere.herokuapp.com/';
+
+        let url = 'http://newsapi.org/v2/everything?' +
+            `q=${query}&` +
+            `from=${Date.now()}&` +
+            'sortBy=popularity&' +
+            'apiKey=9c0a6f3604e64d3eb2c3d50d20dd0770';
+        //http://newsapi.org/v2/everything?q=Amazon&from=2020-09-02&sortBy=popularity&apiKey=9c0a6f3604e64d3eb2c3d50d20dd0770
+
+        this.setState({
+            api: url
+        })
+
+        var articles = [];
+
+        let req = new Request(proxy + url);
+        await fetch(req)
+            .then(
+                function (response) {
+                    // console.log(response.json());
+                    return response.json();
+                }
+            )
+            .then(
+                function (data) {
+                    var count = 0;
+                    for (var key in data['articles']) {
+                        if (count > 10) {
+                            break;
+                        }
+                        article = new Object();
+                        article.author = data['articles'][key]['author'];
+                        article.title = data['articles'][key]['title'];
+                        article.description = data['articles'][key]['description'];
+                        article.url = data['articles'][key]['url'];
+                        article.urlToImage = data['articles'][key]['urlToImage'];
+                        article.publishedAt = data['articles'][key]['publishedAt'];
+                        article.content = data['articles'][key]['content'];
+
+                        articles.push(article);
+                        count++;
+                    }
+                    pointerToThis.setState({
+                        arr: articles
+                    })
+                }
+            )
+    }
+
     render() {
+        var index = 0; // Dynamically set the index at the end of every card 'array' access.
+        const { api, arr } = this.state;
         return (
             <div>
-                <h2>Page Under Development</h2>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer scelerisque venenatis dui, vitae viverra tortor consequat vel. Cras facilisis felis a pellentesque pretium. Cras imperdiet nunc nulla, et facilisis tortor fermentum eget. Integer dignissim, nibh eget accumsan consequat, eros lectus facilisis arcu, non scelerisque libero lacus ac felis. Aenean eget ipsum lacus. Duis condimentum quam ac nunc euismod, sit amet volutpat nibh dignissim. Praesent elementum malesuada mi, at molestie tellus. Nulla orci metus, gravida vitae vehicula a, tristique ac justo. Donec viverra nec dui ac vulputate. Mauris fringilla feugiat ligula, vitae pretium nulla molestie sed. Duis porttitor ex non ex rhoncus, sed sodales urna placerat. Sed euismod leo a libero bibendum tempor. Suspendisse imperdiet fermentum semper. Sed vitae urna a turpis sollicitudin dignissim. Nulla sed eleifend dolor, at iaculis mauris. Morbi id felis vitae mi iaculis eleifend vitae et odio.
-
-                    In venenatis ultricies nisi, eu varius risus volutpat ac. Nunc consectetur convallis imperdiet. Phasellus tempus nisi et dignissim congue. Donec gravida, velit vel feugiat ultrices, ante nulla pretium lectus, fringilla tincidunt arcu dolor id quam. Morbi ipsum tortor, rutrum ac convallis eu, gravida ac tellus. Morbi vitae sapien rutrum, congue lectus vitae, pharetra orci. Vestibulum et purus id ipsum varius hendrerit vitae et sapien. Vivamus nec viverra nisi, eget mattis justo. Maecenas viverra massa sed lacus bibendum bibendum. Nulla placerat tristique congue. Ut sagittis elit vitae rutrum malesuada.
-
-                    Ut ut dictum augue. Nunc ultricies augue nec ante molestie, in efficitur tellus tristique. Praesent sagittis neque viverra nibh fringilla, at suscipit lacus condimentum. Pellentesque quis justo sit amet orci elementum aliquet id id nisl. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed posuere dui eu tristique varius. Nullam nulla justo, mattis eu porta eget, gravida eu orci. Donec vitae suscipit orci, ut dignissim lorem.
-
-                    Donec vestibulum, felis et laoreet gravida, magna velit mollis tellus, dignissim tristique metus ante non odio. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aenean ullamcorper massa ex, pharetra laoreet orci posuere nec. Fusce egestas vestibulum diam non tincidunt. Aenean quis nisl ante. Donec nec nisl blandit, gravida elit quis, ultrices turpis. Vivamus ac ullamcorper nisi. Suspendisse vehicula at est ut mattis. Nullam velit mi, posuere nec arcu eu, scelerisque mattis eros. Maecenas varius ante ut ullamcorper congue. Maecenas ante urna, lacinia et turpis auctor, cursus laoreet eros. Aliquam nisi neque, volutpat sit amet ullamcorper ac, tempor sed massa. Praesent urna odio, tristique non cursus ut, aliquet ac nisl.
-
-                    Mauris arcu lorem, tempus fermentum massa vitae, venenatis rhoncus augue. Nulla a mattis nisi. Sed vestibulum a erat in facilisis. Nam consequat tortor mollis quam pharetra, in vehicula massa varius. Sed hendrerit sapien nulla. Vivamus vitae feugiat nisl. Phasellus malesuada ut enim sed maximus. Integer et velit eget leo congue tempor. Nulla placerat lacus ut tincidunt interdum. Nulla hendrerit sollicitudin mauris, vel fringilla nibh sodales hendrerit. Morbi nec justo nibh. Duis porta metus vitae lectus eleifend, vel commodo metus luctus. Duis a viverra metus. Nullam molestie libero sit amet lacus consequat, ac finibus nulla tincidunt.
-                 </p>
-            </div>
-        );
+                <h2> Page Under Development </h2>
+                <div className="recommended-title">
+                    {<h2>{"API: " + api}</h2>}
+                    {<h1>Query:{this.props.changeLink}</h1>}
+                    <h1>Recommended</h1>
+                </div>
+                <div className="news-post-body">
+                    <div className="news-post-card">
+                        <a href={arr[index] === undefined ? '' : arr[index].url} target="_blank">
+                            <div className="news-post-card-image">
+                                <img src={arr[index] === undefined ? '' : arr[index].urlToImage}></img>
+                            </div>
+                        </a>
+                        <div className="news-post-card-text">
+                            <span className="date">{arr[index] === undefined ? '' : arr[index].publishedAt}</span>
+                            <h2>{arr[index] === undefined ? '' : arr[index].title}</h2>
+                            <p>{arr[index] === undefined ? '' : arr[index++].description}</p>
+                        </div>
+                    </div >
+                </div>
+            </div >
+        )
     }
 }
