6fba697a7d7993ef6c3f4b41e46f5256e6a1e4be
Tejan C <tejan_rs@hotmail.com>
2020-10-19 23:11:05 -0400

    Fixed errors

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,14 +6,15 @@ export default class About extends React.Component {
             <div>
                 <br></br>
                 <h1>ABOUT</h1>
-                <br></br>
                 <p>
                     <br></br>
-                    Stocks app is a website where users interested in the financial securities or stocks can have access to historical data on stocks.<br></br>
-                    The page consists of a search tool, plot section and, and relevant news section. Users interested in the news can also use the News<br></br>
-                    tab to navigate to most popular news throughout their visit of our website.
+                    This is a website where users interested in the financial securities or stocks can have access to historical data on stocks.<br></br>
+                    The page consists of a search tool, plot section, and a relevant news section. Users interested in the news can also use the News<br></br>
+                    navigation tab to browse through the most popular news throughout their visit of our website.
                     <br></br>
-                    This website also allows readers to view news articles with a responsive blog post style. Feel free to browse around with the news blog articles.
+                    <br></br>
+                    Our website also allows readers to view news articles with a responsive blog post style. Feel free to browse around with the news blog articles.
+                    <br></br>
                     <br></br>
                     We are working to make this accessible and free for everyone to use permanently.
                     If you have any questions or concerns, please do not hesitate to contact us using our Contact Form.
