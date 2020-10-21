66c6ade7e44e81104e15373210df36db1b17e16d
Tejan C <tejan_rs@hotmail.com>
2020-10-21 00:09:20 -0400

    Additional summary

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,12 +8,16 @@ export default class About extends React.Component {
                 <h1>ABOUT</h1>
                 <p>
                     <br></br>
-                    This is a website where users interested in financial securities or stocks can access historical data.<br></br>
+                    This is a website where users interested in financial securities or stocks can access its historical data.<br></br>
                     The page consists of a search tool, plot section, and a relevant news section. Users interested in news can also use the News<br></br>
                     tab to browse through most popular updates throughout their visit of our website.<br></br>
                     <br></br>
+                    This website is aimed both at those who are interested in attractive companies for long-term investment and <br></br>
+                    at those who want to make a short-term deal. However, this website does not verify the accuracy of the data nor<br></br>
+                    does it endorse the providers of the data. Please use your own best judgement while making decisions in investments.<br></br>
+                    <br></br>
                     We are working to make this accessible and free for everyone to use permanently. If you have any questions or concerns, please<br></br>
-                    do not hesitate to contact us using our Contact Form.
+                    do not hesitate to contact us by using our Contact Form.
                 </p>
                 <br></br>
                 <h3>Created by</h3>
